ac9fc5e101496dc317f15a58346ce1dc841d573e
evgenii20 <zev_ps@mail.ru>
2021-07-04 08:20:30 +0500

    lesson_4_hw

--- lesson_3_hw/client.py
+++ lesson_3_hw/client.py
@@ -1,77 +1,15 @@
 import json
-import os
 import sys
 import socket
 import time
 
-# from socket import *
+# импорт модуля utils
+from utils import load_configs, get_message, send_message
 
 # объявдяем константу типа словарь
-from socket import socketpair
+# from socket import socketpair
 
 CONFIGS = dict()
-
-
-def load_config(is_server=True):
-    """Загрузка файла конфигурации"""
-
-    config_keys = [
-        'DEFAULT_PORT',
-        'MAX_CONNECTIONS',
-        'MAX_PACKAGE_LENGTH',
-        'ENCODING',
-        'ACTION',
-        'TIME',
-        'USER',
-        'ACCOUNT_NAME',
-        'PRESENCE',
-        'RESPONSE',
-        'ERROR'
-    ]
-    # если не сервер, то добавляем "DEFAULT_IP_ADDRESS"
-    if not is_server:
-        config_keys.append('DEFAULT_IP_ADDRESS')
-    # если файл не найден: сообщение
-    if not os.path.exists('config.json'):
-        print('Файл конфигурации не найден')
-        sys.exit(1)
-    # если файл найден, отрываем для чтения настроек
-    with open('config.json') as config_file:
-        # записываем в глообальную константу-словарь CONFIGS
-        CONFIGS = json.load(config_file)
-    load_config_keys = list(CONFIGS.keys())
-    # проверяем корректность списка ключей
-    for key in config_keys:
-        # если ключ не в списке загруженных ключей, то уведомление
-        if key not in load_config_keys:
-            print(f'В файле конфигурации не хватает ключа: {key}')
-            sys.exit(1)
-    return CONFIGS
-
-
-def get_message(opened_socket, CONFIGS):
-    """Получение сообщений от клиента к серверу и наоборот"""
-    # получаем пакет данных
-    response = opened_socket.recv(CONFIGS.get('MAX_PACKAGE_LENGTH'))
-    # если экземпляр класса response в байтах, то
-    if isinstance(response, bytes):
-        json_response = response.decode(CONFIGS.get('ENCODING'))
-        response_dict = json.loads(json_response)
-        # если экземпляр класса словаря словарь, то возвращаем его, иначе Err
-        if isinstance(response_dict, dict):
-            return response_dict
-        raise ValueError
-    raise ValueError
-
-
-def send_message(opened_socket, message, CONFIGS):
-    """Отправка сообщений от клиента к серверу и наоборот"""
-    # читаем сообщение JSON
-    json_message = json.dumps(message)
-    # формируем ответ (response)
-    response = json_message.encode(CONFIGS.get('ENCODING'))
-    # response = json_message.encode('utf-8')
-    opened_socket.send(response)
 
 
 def handle_response(message):
@@ -79,8 +17,9 @@ def handle_response(message):
     if CONFIGS.get('RESPONSE') in message:
         if message[CONFIGS.get('RESPONSE')] == 200:
             return '200 : OK'
-        return f'400 : {message[CONFIGS.get("ERROR")]}'
-    raise ValueError
+        if message[CONFIGS.get('RESPONSE')] == 400:
+            return f'400 : {message[CONFIGS.get("ERROR")]}'
+    # raise ValueError
 
 
 def create_presence_message(account_name):
@@ -111,7 +50,7 @@ def main():
     # блок запуска клиента
     global CONFIGS
     # is_server=False - клиент не сервер
-    CONFIGS = load_config(is_server=False)
+    CONFIGS = load_configs(is_server=False)
     try:
         # server_address = CONFIGS.get('DEFAULT_IP_ADDRESS')
         # разбираем строку запуска клиента на части в виде списка:
--- lesson_3_hw/server.py
+++ lesson_3_hw/server.py
@@ -3,70 +3,11 @@ import os
 import sys
 import socket
 
+# импорт модуля utils
+from utils import load_configs, get_message, send_message
+
 # объявдяем словарь для конфигурации
 CONFIGS = dict()
-
-
-def load_config(is_server=True):
-    """Загрузка файла конфигурации"""
-
-    config_keys = [
-        'DEFAULT_PORT',
-        'MAX_CONNECTIONS',
-        'MAX_PACKAGE_LENGTH',
-        'ENCODING',
-        'ACTION',
-        'TIME',
-        'USER',
-        'ACCOUNT_NAME',
-        'PRESENCE',
-        'RESPONSE',
-        'ERROR'
-    ]
-    # если не сервер, то добавляем "DEFAULT_IP_ADDRESS"
-    if not is_server:
-        config_keys.append('DEFAULT_IP_ADDRESS')
-    # если файл не найден: сообщение
-    if not os.path.exists('config.json'):
-        print('Файл конфигурации не найден')
-        sys.exit(1)
-    # если файл найден, отрываем для чтения настроек
-    with open('config.json') as config_file:
-        # записываем в глообальную константу-словарь CONFIGS
-        CONFIGS = json.load(config_file)
-    load_config_keys = list(CONFIGS.keys())
-    # проверяем корректность списка ключей
-    for key in config_keys:
-        # если ключ не в списке загруженных ключей, то уведомление
-        if key not in load_config_keys:
-            print(f'В файле конфигурации не хватает ключа: {key}')
-            sys.exit(1)
-    return CONFIGS
-
-
-def get_message(opened_socket, CONFIGS):
-    """Получение сообщений от клиента к серверу и наоборот"""
-    # получаем пакет данных
-    response = opened_socket.recv(CONFIGS.get('MAX_PACKAGE_LENGTH'))
-    # если экземпляр класса response в байтах, то
-    if isinstance(response, bytes):
-        json_response = response.decode(CONFIGS.get('ENCODING'))
-        response_dict = json.loads(json_response)
-        # если экземпляр класса словаря словарь, то возвращаем его, иначе Err
-        if isinstance(response_dict, dict):
-            return response_dict
-        raise ValueError
-    raise ValueError
-
-
-def send_message(opened_socket, message, CONFIGS):
-    """Отправка сообщений от клиента к серверу и наоборот"""
-    # читаем сообщение JSON
-    json_message = json.dumps(message)
-    # формируем ответ (response)
-    response = json_message.encode(CONFIGS.get('ENCODING'))
-    # response = json_message.encode('utf-8')
-    opened_socket.send(response)
 
 
 def handle_response(message):
@@ -87,7 +28,7 @@ def handle_response(message):
 def main():
     # блок запуска сервера
     global CONFIGS
-    CONFIGS = load_config()
+    CONFIGS = load_configs()
     try:
         # обработка параметра командной строки
         if '-p' in sys.argv:
